9f7b3454b425bd2a8fa301ebb7f72e48f25f75bf
Evgeniya <osipova.dr17@yandex.ru>
2022-12-26 20:50:20 +0900

    Добавление действия выбора исполнителя

--- classes/Task.php
+++ classes/Task.php
@@ -4,23 +4,24 @@ class Task
 {
     public const STATUS_NEW = 'new';
     public const STATUS_CANCEL = 'cancelled';
+    public const STATUS_WAIT = 'waiting'; // мжн ли доваить новый статус типо в ожидании? ответа на принятие от заказчика? или new оставить?
     public const STATUS_WORK = 'at_work';
     public const STATUS_DONE = 'done';
     public const STATUS_FAIL = 'failed';
 
     public const ACTION_CANCEL = 'cancel';
     public const ACTION_RESPOND = 'respond';
+    public const ACTION_ACCEPT = 'accept';
     public const ACTION_DONE = 'done';
     public const ACTION_DENY = 'deny';
 
-    private $customer_id;
-    private $implementer_id;
+    private $customer_id = 1; // типо пока нет базы данных можно присвоить id?
+    private $implementer_id = 2;
     private $current_status;
 
-    public function __construct($customer_id, $implementer_id, $current_status)
+    public function __construct($current_id, $current_status)
     {
-        $this->customer_id = $customer_id;
-        $this->implementer_id = $implementer_id;
+        $this->current_id = $current_id;
         $this->current_status = $current_status;
     }
 
@@ -29,6 +30,7 @@ class Task
         return [
             self::STATUS_NEW => 'Новое',
             self::STATUS_CANCEL => 'Отменено',
+            self::STATUS_WAIT => 'В ожидании',
             self::STATUS_WORK => 'В работе',
             self::STATUS_DONE => 'Выполнено',
             self::STATUS_FAIL => 'Провалено'
@@ -40,6 +42,7 @@ class Task
         return [
             self::ACTION_CANCEL => 'Отменить',
             self::ACTION_RESPOND => 'Откликнуться',
+            self::ACTION_ACCEPT => 'Принять',
             self::ACTION_DONE => 'Выполнено',
             self::ACTION_DENY => 'Отказаться'
         ];
@@ -47,9 +50,10 @@ class Task
 
     public function getAvailableAction($current_id, $current_status): string
     {
-        if ($current_id === $this->customer_id) {
+        if ($current_id === $this->customer_id) { // с какими данными будет сравниваться current_id
             $availableAction = [
                 self::STATUS_NEW => self::ACTION_CANCEL,
+                self::STATUS_WAIT => self::ACTION_ACCEPT,
                 self::STATUS_WORK => self::ACTION_DONE
             ];
             return $availableAction[$current_status];
@@ -68,7 +72,8 @@ class Task
     {
         $newStatus = [
             self::ACTION_CANCEL => self::STATUS_CANCEL,
-            self::ACTION_RESPOND => self::STATUS_WORK,
+            self::ACTION_RESPOND => self::STATUS_WAIT,
+            self::ACTION_ACCEPT => self::STATUS_WORK,
             self::ACTION_DONE => self::STATUS_DONE,
             self::ACTION_DENY => self::STATUS_FAIL
         ];
--- index.php
+++ index.php
@@ -1,15 +1,17 @@
 <?php
 require_once 'classes/Task.php';
 
-$task = new Task(1, 2, 'new');
+$task = new Task(1, 'new');
 
 assert($task->getNewStatus(Task::ACTION_CANCEL) === Task::STATUS_CANCEL, 'cancel');
-assert($task->getNewStatus(Task::ACTION_RESPOND) === Task::STATUS_WORK, 'at_work');
+assert($task->getNewStatus(Task::ACTION_RESPOND) === Task::STATUS_WAIT, 'waiting');
+assert($task->getNewStatus(Task::ACTION_ACCEPT) === Task::STATUS_WORK, 'at_work');
 assert($task->getNewStatus(Task::ACTION_DONE) === Task::STATUS_DONE, 'done');
 assert($task->getNewStatus(Task::ACTION_DENY) === Task::STATUS_FAIL, 'failed');
 
 assert($task->getAvailableAction(1, Task::STATUS_NEW) === Task::ACTION_CANCEL, 'cancel');
 assert($task->getAvailableAction(2, Task::STATUS_NEW) === Task::ACTION_RESPOND, 'respond');
+assert($task->getAvailableAction(1, Task::STATUS_WAIT) === Task::ACTION_ACCEPT, 'accept');
 
 assert($task->getAvailableAction(1, Task::STATUS_WORK) === Task::ACTION_DONE, 'done');
 assert($task->getAvailableAction(2, Task::STATUS_WORK) === Task::ACTION_DENY, 'failed');
